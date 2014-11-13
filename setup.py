032618e504aa4e9152137b0a78223ed929cd0d15
Fabrizio Lungo <fab@lungo.co.uk>
2014-11-13 11:50:00 +0000

    Modified to support download_url

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from distutils.core import setup
+from cms.utils.plugins import downcast_plugins
+
+VERSION = '1.0.0'
+GIT_URL = 'https://github.com/flungo/django_cms_auth_apphook'
 
 setup(
     name='django_cms_auth_apphook',
-    version='1.0.0',
+    version=VERSION,
     packages=['auth_apphook'],
-    url='https://github.com/flungo/django_cms_auth_apphook',
+    url=GIT_URL,
+    download_url= GIT_URL + "/tarball/" + VERSION,
     license='MIT',
     author='Fabrizio Lungo',
     author_email='fab@lungo.co.uk',
