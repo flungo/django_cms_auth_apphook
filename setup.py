4b01de4d764f72f8b96fe36cc626c5f3a46105de
Fabrizio Lungo <fab@lungo.co.uk>
2014-11-13 08:56:00 +0000

    Created simple setup.py install script.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='django_cms_auth_apphook',
+    version='1.0.0',
+    packages=['auth_apphook'],
+    url='https://github.com/flungo/django_cms_auth_apphook',
+    license='MIT',
+    author='Fabrizio Lungo',
+    author_email='fab@lungo.co.uk',
+    description='App hook to make integration with the standard Django Auth Library easier. Also includes templates for all auth views.'
+)
